df05638fb35fcc400b4403aa33cae18cb99cb526
Raji <rajalakshmiaruna2005@gmail.com>
2025-07-21 11:17:06 +0530

    fix: configure PeerJS with default cloud server and update peer ID display

--- src/components/peer.jsx
+++ src/components/peer.jsx
@@ -1,11 +1,5 @@
 // src/utils/peer.js
 import Peer from 'peerjs';
-
-export const createPeer = (id = undefined) => {
- 
-  return new Peer(id, {
-    host: '192.168.121.242',
-    port: 9000,
-    path: '/',
-  }); // uses PeerJS default cloud server
-};
+// uses PeerJS default cloud server
+const peer = new Peer(); // Uses public PeerJS cloud server
+export default peer;
